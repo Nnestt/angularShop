e510721442fb23d04728ad3e57b3d4fdaecec1b4
Ernest <ernest36912@gmail.com>
2021-08-12 18:00:58 +0800

    initial setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: { 
+    apiKey: "AIzaSyAnLWgObq8Ha__c6J4qmvN3jvEEN5FEaCg",
+    authDomain: "shopapp-3a70e.firebaseapp.com",
+    projectId: "shopapp-3a70e",
+    storageBucket: "shopapp-3a70e.appspot.com",
+    messagingSenderId: "521646993253",
+    appId: "1:521646993253:web:299b6c4de4cea264dcfb8c"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: { 
+    apiKey: "AIzaSyAnLWgObq8Ha__c6J4qmvN3jvEEN5FEaCg",
+    authDomain: "shopapp-3a70e.firebaseapp.com",
+    projectId: "shopapp-3a70e",
+    storageBucket: "shopapp-3a70e.appspot.com",
+    messagingSenderId: "521646993253",
+    appId: "1:521646993253:web:299b6c4de4cea264dcfb8c"
+  }
 };
 
 /*
